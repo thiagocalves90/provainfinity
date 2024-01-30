3a7c9887291748624f3a72ef31e9a14d17da95f6
thiagocalves90 <158157031+thiagocalves90@users.noreply.github.com>
2024-01-30 07:57:42 -0300

    Create provaaula1.py

---
+++ provaaula1.py
@@ -0,0 +1,11 @@
+#Tipo int
+idade = 33
+#tipo float
+altura = 1.75
+#tipo bool
+masculino = True
+#tipo str
+nome = "Thiago"
+
+nome_usuario = input("Informe seu nome: ")
+print(f"Olá {nome_usuario}, seja bem vindo!")
