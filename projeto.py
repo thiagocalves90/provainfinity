a29ae43943f4a5d0ed0744071b421d5c8dc98412
thiagocalves90 <158157031+thiagocalves90@users.noreply.github.com>
2024-02-13 12:31:56 -0300

    Update projeto.py

--- projeto.py
+++ projeto.py
@@ -1,1 +1,14 @@
+tentativa = 3
 
+for i in range(tentativa):
+    senha = input("Digite sua senha: ")
+    confirmacao_senha = input("Confirme sua senha: ")
+    if senha == confirmacao_senha:
+        print("Senha correta. Bem vindo.")
+        break
+    else:    
+        tentativa_restante = tentativa - i - 1
+        print(f"Senha incorreta. Você tem {tentativa_restante} até o bloqueio.")
+
+if i == tentativa - 1:
+    print(f"Telefone bloquado. Entre em contato com o suporte.")
